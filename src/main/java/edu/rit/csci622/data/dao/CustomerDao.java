0a53fda9b59d25f2f4072a7084ece020992960ed
Nathaniel <ncotton6@gmail.com>
2016-03-21 15:19:28 -0400

    filled in the rest of the sps for customer

--- src/main/java/edu/rit/csci622/data/dao/CustomerDao.java
+++ src/main/java/edu/rit/csci622/data/dao/CustomerDao.java
@@ -13,20 +13,20 @@ public interface CustomerDao {
 	public int createUser(@Param("user") User user, @Param("key") String key);
 
 	public User getUser(@Param("userId") int userId, @Param("key") String key);
-	
+
 	public void createSession(@Param("uuid") String uuid, @Param("userId") int userId, @Param("key") String key);
-	
+
 	public List<Product> getProduct(@Param("productId") int productId, @Param("key") String key);
-	
-	public int placeOrder(@Param("productId") int productId, @Param("qty") int quantity, @Param("userId") int userId, @Param("key") String key);
-	
+
+	public int placeOrder(@Param("productId") int productId, @Param("qty") int quantity, @Param("userId") int userId,
+			@Param("key") String key);
+
 	public List<Product> getProducts(@Param("key") String key);
-	
+
 	public int requestHire(@Param("userId") int userId, @Param("roleId") int roleId);
-	
+
 	public void deleteSession(@Param("sessionId") int sessionId);
-	
-	public Map<String,String> getUserPassword(@Param("username") String username, @Param("key") String key);
-	
+
+	public Map<String, String> getUserPassword(@Param("username") String username, @Param("key") String key);
 
 }
