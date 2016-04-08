c9edf3814a4893bb39e292cf484e48acfca365bb
Nathaniel <ncotton6@gmail.com>
2016-04-08 19:03:09 -0400

    login working in trivial case

--- src/main/java/edu/rit/csci622/controller/LoginInController.java
+++ src/main/java/edu/rit/csci622/controller/LoginInController.java
@@ -1,9 +1,14 @@
 package edu.rit.csci622.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,19 +19,22 @@ import edu.rit.csci622.auth.Auth;
 @Auth
 public class LoginInController {
 
-	
-	
 	@RequestMapping(method = RequestMethod.GET)
-	public String index(){
+	public String index(Model modal) {
 		return "login";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public String login(HttpServletResponse response){
+	public String login(String username, String password, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		System.out.println("Username: " + username);
+		System.out.println("Password: " + password);
 		Cookie c = new Cookie("ecommsession", "test");
 		c.setMaxAge(3600);
 		response.addCookie(c);
+		String uri = request.getContextPath();
+		response.sendRedirect(uri+"/");
 		return "index";
 	}
-	
+
 }
